72d198f72d3528afd11c30d45e2488e66e5b2900
Joshua Mee <wiggyworm_@hotmail.com>
2020-07-01 15:10:31 +0100

    Setting up our react app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-2",
+      BUCKET: "meejosh-notes-app-uploads"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "https://yg2vmrma4d.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_nKEaFkE3I",
+      APP_CLIENT_ID: "32bvu8905i9qodd19ltitblrsh",
+      IDENTITY_POOL_ID: "us-east-2:a12205b4-324e-411e-a827-eeb84ce94372"
+    }
+  };
