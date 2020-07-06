6d5c2b9b75db5fc90f50291b6d6a591060c0954b
Joshua Mee <wiggyworm_@hotmail.com>
2020-07-06 12:29:03 +0100

    Adding a Netlify build script

--- src/config.js
+++ src/config.js
@@ -59,9 +59,3 @@ export default {
   const config = process.env.REACT_APP_STAGE === 'prod'
     ? prod
     : dev;
-  
-  export default {
-    // Add common config values here
-    MAX_ATTACHMENT_SIZE: 5000000,
-    ...config
-  };
